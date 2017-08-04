17b66ba410de46a17972ee86e873c5b611a6078e
Sanjeevi <prasanjeevi@gmail.com>
2017-08-04 15:56:54 +0530

    spring init

---
+++ src/main/java/com/cloudier/configclientapp/ConfigClientAppApplication.java
@@ -0,0 +1,12 @@
+package com.cloudier.configclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAppApplication.class, args);
+	}
+}
